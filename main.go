65930aabfde9e0bc41f78e534c290440082d9884
Antoine <ahuret@skilld.fr>
2017-07-20 13:48:34 +0200

    fixes

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
-package main 
+package odoo 
 
 import (
 	"fmt"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/antony360/go-odoo/odoo"
 )
 
 func main() {
-	c, err := odoo.NewClient("http://62.210.250.198:8069", nil)
+	c, err := NewClient("http://62.210.250.198:8069", nil)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
